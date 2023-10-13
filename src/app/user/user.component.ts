408896f3223997ce8664894cbe6f4cf34768e27f
RobinB87 <robinbette@hotmail.com>
2023-10-13 07:46:53 +0000

    add with scan operator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { UserService } from '../core/services/user.service';
+import { Observable, Subject, from, merge, of, scan } from 'rxjs';
+
 import { User } from '../core/models/user';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -11,5 +12,27 @@ import { Observable } from 'rxjs';
 export class UserComponent {
   users$: Observable<User[]> = this.userService.get();
 
+  private userAddSubject: Subject<User> = new Subject();
+  userAdd$: Observable<User> = this.userAddSubject.asObservable();
+
+  usersWithAdd$: Observable<User[]> = merge(this.users$, this.userAdd$).pipe(
+    scan(
+      (acc, value) => (value instanceof Array ? [...value] : [...acc, value]),
+      [] as User[]
+    )
+  );
+
   constructor(private readonly userService: UserService) {}
+
+  add(): void {
+    this.userAddSubject.next(this.createUser());
+  }
+
+  private createUser(): User {
+    return {
+      id: 11,
+      name: 'Johnyboi',
+      email: 'bla@bla.com',
+    } as User;
+  }
 }
