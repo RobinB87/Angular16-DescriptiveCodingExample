1da34caade55546434a8c1465ad2571e8e24eaba
RobinB87 <robinbette@hotmail.com>
2023-10-13 07:55:10 +0000

    refactor to service

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject, merge, scan } from 'rxjs';
 
 import { User } from '../models/user';
 
@@ -10,9 +10,32 @@ import { User } from '../models/user';
 export class UserService {
   api: string = 'https://jsonplaceholder.typicode.com';
 
+  private userAddSubject: Subject<User> = new Subject();
+  userAdd$: Observable<User> = this.userAddSubject.asObservable();
+
+  private users$: Observable<User[]> = this.get();
+  usersWithAdd$: Observable<User[]> = merge(this.users$, this.userAdd$).pipe(
+    scan(
+      (acc, value) => (value instanceof Array ? [...value] : [...acc, value]),
+      [] as User[]
+    )
+  );
+
   constructor(private readonly httpClient: HttpClient) {}
 
   get(): Observable<User[]> {
     return this.httpClient.get<User[]>(`${this.api}/users`);
+  }
+
+  add(user?: User): void {
+    this.userAddSubject.next(user ? user : this.createFakeUser());
+  }
+
+  private createFakeUser(): User {
+    return {
+      id: 11,
+      name: 'Johnyboi',
+      email: 'bla@bla.com',
+    } as User;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { UserService } from '../core/services/user.service';
-import { Observable, Subject, from, merge, of, scan } from 'rxjs';
+import { Observable } from 'rxjs';
 
+import { UserService } from '../core/services/user.service';
 import { User } from '../core/models/user';
 
 @Component({
@@ -10,29 +10,9 @@ import { User } from '../core/models/user';
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
-  users$: Observable<User[]> = this.userService.get();
-
-  private userAddSubject: Subject<User> = new Subject();
-  userAdd$: Observable<User> = this.userAddSubject.asObservable();
-
-  usersWithAdd$: Observable<User[]> = merge(this.users$, this.userAdd$).pipe(
-    scan(
-      (acc, value) => (value instanceof Array ? [...value] : [...acc, value]),
-      [] as User[]
-    )
-  );
+  users$: Observable<User[]> = this.userService.usersWithAdd$;
 
   constructor(private readonly userService: UserService) {}
 
-  add(): void {
-    this.userAddSubject.next(this.createUser());
-  }
-
-  private createUser(): User {
-    return {
-      id: 11,
-      name: 'Johnyboi',
-      email: 'bla@bla.com',
-    } as User;
-  }
+  add = () => this.userService.add();
 }
